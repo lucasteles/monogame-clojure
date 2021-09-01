b9ceae93e152dc67bdae2e5de720d5a5a13006ee
Lucas Teles <lucas.teles@outlook.com>
2021-08-31 23:27:46 -0300

    WIP

--- Program.cs
+++ Program.cs
@@ -2,19 +2,13 @@
 using System.IO;
 using clojure.lang;
 using Microsoft.Xna.Framework;
+using clojure.clr.api;
 
-LoadCljFiles("src/");
-var load = clojure.clr.api.Clojure.var("clrgame.game", "-main");
-load.invoke();
 
-static void LoadCljFiles(string sDir)
-{
-    foreach (var d in Directory.GetDirectories(sDir))
-    foreach (var file in Directory.GetFiles(d, "*.clj"))
-    {
-        var filename = Path.GetFileNameWithoutExtension(file);
-        var path = Path.Combine(Path.GetDirectoryName(file), filename);
-        RT.load(path);
-        LoadCljFiles(d);
-    }
-}
+var load = Clojure.var("clojure.core", "load");
+load.invoke("/src/clrgame/interop");
+load.invoke("/src/clrgame/monogame");
+load.invoke("/src/clrgame/game");
+var run = Clojure.var("clrgame.game", "-main");
+run.invoke();
+
