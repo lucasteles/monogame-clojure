c9fb125d74c12a8dd314625918941d85993887ab
Lucas Teles <lucas.teles@outlook.com>
2021-09-02 18:12:01 -0300

    WIP

---
+++ cljgame/entities/pipes.clj
@@ -0,0 +1,58 @@
+(ns cljgame.entities.pipes
+  (:require [cljgame.monogame :as g]
+            [cljgame.monogame :as g])
+  (:import [System Math]
+           [Microsoft.Xna.Framework Color Vector2]
+           [Microsoft.Xna.Framework.Graphics SpriteEffects]))
+
+(def speed 80)
+(def gap 200)
+(def frequency 5)
+(def scale 2)
+
+(def velocity (g/vect (- speed) 0))
+
+(defn init [game]
+  (let [texture (g/load-texture-2d game "pipe")
+        texture-height (-> texture .Bounds .Height (* scale))
+        texture-width (-> texture .Bounds .Width (* scale))]
+    {:texture texture
+     :pipes []
+     :last-spawn 0
+     :heigt texture-height
+     :width texture-width }))
+
+(defn create-pipe [position height window ]
+  (let [random-top (- (rand 300))]
+    {:position (g/vect (-> window g/width (+ 20)) random-top) 
+     :y-offset random-top}))
+
+(defn spawn-handler
+  [{:keys [position height last-spawn] :as state} window delta-time]
+  (if (<= last-spawn frequency)
+    (assoc state :last-spawn (+ last-spawn delta-time))
+    (->  state
+        (update :pipes conj (create-pipe position height window))
+        (assoc :last-spawn 0))))
+
+(defn move-pipe [delta-time pipe] (update pipe :position g/vect+ (g/vect* velocity delta-time)))
+(defn move-pipes [pipes delta-time] (map (partial move-pipe delta-time) pipes))
+
+(defn update- [{:keys [offset width position] :as state} delta-time window]
+  (-> state
+      (spawn-handler window delta-time)
+      (update :pipes move-pipes delta-time)))
+
+(defn draw [sprite-batch {:keys [texture pipes]}]
+  (doseq [pipe pipes]
+    (let [{:keys [position]} pipe]
+      (println position)
+      (g/draw sprite-batch {:texture texture
+                            :position position
+                            :source-rectangle (.Bounds texture)
+                            :color Color/White
+                            :rotation 0
+                            :origin Vector2/Zero
+                            :scale scale
+                            :effects SpriteEffects/None
+                            :layer-depth 0}))))
--- cljgame/game.clj
+++ cljgame/game.clj
@@ -2,6 +2,7 @@
   (:require [cljgame.interop :refer [current current-exe-dir int32 load-monogame]]
             [cljgame.monogame :as g] 
             [cljgame.entities.floor :as floor]
+            [cljgame.entities.pipes :as pipes]
             [cljgame.entities.background :as background])
   (:import [System Console]
            [Microsoft.Xna.Framework Color Vector2]
@@ -23,20 +24,23 @@
   (game-configuration! game graphics)
 
   {:floor (floor/init game window) 
-   :background (background/init game window) }) 
+   :background (background/init game window) 
+   :pipes (pipes/init game) }) 
 
 (defn update- [{:keys [delta-time state game window] }]
   (exit-on-esc game)
   (-> state
       (update :background background/update- delta-time)
-      (update :floor floor/update- delta-time)))
+      (update :floor floor/update- delta-time)
+      (update :pipes pipes/update- delta-time window)))
 
 (defn draw [{:keys [sprite-batch graphics-device window]
-             { :keys [floor background] } :state}]
+             { :keys [floor background pipes] } :state}]
     (g/clear graphics-device Color/LightGray)
     (g/begin sprite-batch)
     (background/draw sprite-batch background)
     (floor/draw sprite-batch floor)
+    (pipes/draw sprite-batch pipes)
     (g/end sprite-batch))
 
 (defn -main [& args]
